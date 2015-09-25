92c643a38a9d3efe7c8c1b82cf651a9d87d4ce61
D. Potikanond <unknown-user@10.10.182.20>
2015-09-25 14:01:58 +0700

    add Lab602

---
+++ week6/Lab601/README.md
@@ -0,0 +1,13 @@
+﻿# Lab601: Idenfity design patern and participants from code (group of 2 students)
+
+In this lab, each group of 2 students has to identify a design pattern and all participants 
+from the provided C# source code. 
+
+## Submission: a written report which contains
+
+1. A class diagram of the original source code
+2. Detail explaination about the identified pattern and all the parcipants
+3. Explain how to include an asian herbivore and an asian carnivore to the program: 
+	3.1 A class diagram of the program after including the new animals.
+	3.2 Show the snippet of C# code that is related to the process.
+
---
+++ week6/Lab602/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace Lab6
+{
+  class MainApp
+  {
+    static void Main()
+    {
+      Document[] documents = new Document[2];
+ 
+      documents[0] = new Resume();
+      documents[1] = new Report();
+ 
+      // Display document pages
+      foreach (Document document in documents)
+      {
+        Console.WriteLine("\n" + document.GetType().Name + "--");
+        foreach (Page page in document.Pages)
+        {
+          Console.WriteLine(" " + page.GetType().Name);
+        }
+      }
+ 
+      // Wait for user
+      Console.ReadKey();
+    }
+  }
+ 
+  abstract class Page
+  {
+  }
+ 
+  class SkillsPage : Page
+  {
+  }
+ 
+  class EducationPage : Page
+  {
+  }
+ 
+  class ExperiencePage : Page
+  {
+  }
+ 
+  class IntroductionPage : Page
+  {
+  }
+ 
+  class ResultsPage : Page
+  {
+  }
+ 
+  class ConclusionPage : Page
+  {
+  }
+ 
+  class SummaryPage : Page
+  {
+  }
+ 
+  class BibliographyPage : Page
+  {
+  }
+ 
+  abstract class Document
+  {
+    private List<Page> _pages = new List<Page>();
+ 
+    public Document()
+    {
+      this.CreatePages();
+    }
+ 
+    public List<Page> Pages
+    {
+      get { return _pages; }
+    }
+ 
+    public abstract void CreatePages();
+  }
+ 
+  class Resume : Document
+  {
+    public override void CreatePages()
+    {
+      Pages.Add(new SkillsPage());
+      Pages.Add(new EducationPage());
+      Pages.Add(new ExperiencePage());
+    }
+  }
+ 
+  class Report : Document
+  {
+    public override void CreatePages()
+    {
+      Pages.Add(new IntroductionPage());
+      Pages.Add(new ResultsPage());
+      Pages.Add(new ConclusionPage());
+      Pages.Add(new SummaryPage());
+      Pages.Add(new BibliographyPage());
+    }
+  }
+}
+
+
